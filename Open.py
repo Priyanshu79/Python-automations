31f02905b7e1c200a10b53f944befeef9d720858
Priyanshu Mishra <67860164+Priyanshu79@users.noreply.github.com>
2020-11-24 12:18:31 +0530

    Update Open.py

--- Open.py
+++ Open.py
@@ -1,28 +1,24 @@
 import os
 import subprocess
 
-path1='C:\Program Filea\SumatraPDF'  ##path of sumatra pdf, bruh
-path2='C:\Program Files (x86)\PDFlite'  ##Path of pdflite
+path1='C:\Program Files\SumatraPDF'  ##path of sumatra pdf, bruh, my prefered pdf reader when reading multiple pdfs
 
 path3='C:/Users/HP/Desktop/Folder'  ##path where your folders whose files are to be opened exist
 
 Folder1_name="PDF_list_2"  ##Folder_name
 Folder2_name="PDF_list_1"  ##Folder_name
 
-x=int(input("Enter:\n 1 to open all files in PDF_list_2 with Sumatra PDF\n 2 to open all files in PDF_list_1 with PDFlite\n 3 to open all files in a folder in PDF_list_1 with PDFlite\n"))
+x=int(input("Enter:\n 1 to open all files in PDF_list_2 \n 2 to open all files in PDF_list_1\n 3 to open all files in a folder in PDF_list_1\n"))
+os.chdir(path1)
 
 if x==1:
-    os.chdir(path1)
     for file in os.listdir(f"{path3}/PDF_list_2"):
         subprocess.Popen(['SumatraPDF.exe',f'{path3}/{Folder1_name}/file'])  
 elif x==2:
-    os.chdir(path2)
     for root, dirs, files in os.walk(f"{path3}/{Folder2_name}",topdown=False):
         for file in files:
-            subprocess.Popen(['PDFlite.exe',os.path.join(root, file)])  
+            subprocess.Popen(['SumatraPDF.exe',os.path.join(root, file)])  
 elif x==3:
-    os.chdir(path2)
-    #input("Enter the folder name"
     i=1
     dicti={}
     for root, dirs, files in os.walk(f"{path3}/{Folder2_name}",topdown=False):
@@ -34,11 +30,9 @@ elif x==3:
         string+=f"{item} :- {dicti[item]}\n"
     y=int(input(f"\nEnter the corresponding number to the directory whose files you wanna open:\n{string}"))
     for file in os.listdir(f"{path3}/{Folder2_name}/{dicti[y]}"):
-        subprocess.Popen(['PDFlite.exe',f'{path3}/{Folder2_name}/{dicti[y]}/{file}'])  
+        subprocess.Popen(['SumatraPDF.exe',f'{path3}/{Folder2_name}/{dicti[y]}/{file}'])  
 
-    
 
-        
 
 
 
